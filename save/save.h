baa336a7779b1f84577840edb60fdf21374ad0f2
Angélique <angelique.lopes.fr@gmail.com>
2024-11-17 01:53:45 +0100

    first commit save

---
+++ save/save.h
@@ -0,0 +1,13 @@
+#ifndef SAVE_H
+#define SAVE_H
+#include "../load/load.h"
+#include "../menu/menu.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void save(BTree* btree, char* filename);
+
+
+#endif
